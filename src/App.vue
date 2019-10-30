a4d3a8d763cf777bf9d6692cbb52c4049f8b0bb6
MaoErLi <2844735407@qq.com>
2019-10-30 14:53:25 +0800

    vue 初始化

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,9 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <HelloWorld
+      msg="哇偶"
+      desc="aqua 卡哇伊!!!"
+    />
   </div>
 </template>
 
